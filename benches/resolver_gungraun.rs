15cda8a777644a1f612f9f2ca86a39f8ff98b02e
hardfist <yangjianzju@gmail.com>
2026-05-18 15:56:00 +0800

    feat: switch continuous benchmarks to gungraun

---
+++ benches/resolver_gungraun.rs
@@ -0,0 +1,18 @@
+use gungraun::{binary_benchmark, binary_benchmark_group, main, BinaryBenchmarkConfig, Command};
+
+#[binary_benchmark]
+#[bench::resolve_dependencies("deps")]
+#[bench::resolve_many_extensions("many_extensions")]
+#[bench::resolve_dependencies_pnp("pnp")]
+fn bench_resolver(scenario: &str) -> Command {
+  Command::new(env!("CARGO_BIN_EXE_resolver_bench_driver"))
+    .arg(scenario)
+    .build()
+}
+
+binary_benchmark_group!(name = resolver_group, benchmarks = bench_resolver);
+
+main!(
+  config = BinaryBenchmarkConfig::default().env_clear(false),
+  binary_benchmark_groups = resolver_group
+);
